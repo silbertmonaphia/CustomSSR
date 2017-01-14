e45bcce8a650a050ac4e599786942001a854eec9
smona <452293730@qq.com>
2017-01-14 17:27:48 +0800

    优化了终端输出体验，并让-c检测状态选项更加精确

--- rsstart.sh
+++ rsstart.sh
@@ -7,60 +7,60 @@ shadowsocksPath=/home/CustomSSR/shadowsocks/
 configPath=/home/ss/
 
 
-if [ $# -eq 0 ]
+if [ "$#" -eq 0 ]
     then
     echo 'default [Normal Mode]'
     echo 'more detail -h '
     cd $shadowsocksPath && sudo ./local.py -c ${configPath}rss_normal.json -d start
 
-elif [ $1 = "-n" ]
+elif [ "$1" = "-n" ]
     then
     cd $shadowsocksPath && sudo ./local.py -c ${configPath}rss_normal.json -d start
 
-elif [ $1 = "-p" ]
+elif [ "$1" = "-p" ]
     then
     cd $shadowsocksPath && sudo ./local.py -c ${configPath}rss_18+.json -d start
 
-elif [ $1 = '-r' ]
+elif [ "$1" = '-r' ]
     then
-    sudo kill -15  `ps -ef|grep rss_|head -1|awk '{printf $2}'`
+    sudo kill -15  `ps -aux|grep rss_|grep Ss|awk '{printf $2}'`
     cd $shadowsocksPath && sudo ./local.py -c ${configPath}rss_18+.json -d start
 
 
-elif [ $1 = '-b' ]
+elif [ "$1" = '-b' ]
     then
-    sudo kill -15  `ps -ef|grep rss_|head -1|awk '{printf $2}'`
+    sudo kill -15  `ps -aux|grep rss_|grep Ss|awk '{printf $2}'`
     cd $shadowsocksPath && sudo ./local.py -c ${configPath}rss_normal.json -d start
     
-elif [ $1 = '-s' ]
+elif [ "$1" = '-s' ]
     then
     echo 'ssR process stop completely'
-    sudo kill -15  `ps -ef|grep rss_|head -1|awk '{printf $2}'`
+    sudo kill -15  `ps -aux|grep rss_|grep Ss|awk '{printf $2}'`
 
-elif [ $1 = '-c' ]
+elif [ "$1" = '-c' ]
+
     then
-    var=$(ps -ef|grep 'rss_normal.json -d'|head -1|awk '{printf $8}')
-    if [ $var = 'grep' ]
-        then
-        jug=$(ps -ef|grep 'rss_18+.json -d'|head -1|awk '{printf $8}')
-
-        if [ $jug = 'grep' ]
-            then 
-            echo 'you are not start yet'
-        else    
-            echo 'now is 18+ mode'
-        fi
-
-    else
+        var=$(ps -aux|grep 'rss_normal.json -d'|grep Ss|awk '{printf $11}')
+        jug=$(ps -aux|grep 'rss_18+.json -d'|grep Ss|awk '{printf $11}')
+    
+    if  [ "${var}" = 'python' ]
+    then
         echo 'now is normal mode'
+
+    elif  [ "${jug}" = 'python' ]
+    then
+        echo 'now is 18+ mode'
+
+    else    
+        echo 'you are not start yet'
     fi
 
-elif [ $1 = '-v' ]
+elif [ "$1" = '-v' ]
     then
     echo 'normal mode ==>' && cat ${configPath}rss_normal.json|grep '.ipip.pm'
     echo '18+     mode ==>' && cat ${configPath}rss_18+.json|grep '.ipip.pm'
     
-elif [ $1 = "-h" ]
+elif [ "$1" = "-h" ]
     then
     echo 'Help                     : -h'
     echo 'Normal Mode             : -n'
